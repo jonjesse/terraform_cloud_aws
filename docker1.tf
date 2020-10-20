286d24be658f46af46561210439141ac03a31354
jonjesse <root@sys1.attlocal.net>
2020-10-20 15:21:08 -0500

    fixed

--- docker1.tf
+++ docker1.tf
@@ -45,7 +45,7 @@ resource "aws_instance" "docker1" {
   connection {
     type = "ssh"
     user = "ubuntu"
-    private_key = file ("/root/aws/aws5.pem")
+    private_key = file ("aws5")
     host = aws_instance.docker1.public_ip
     }
   }
@@ -57,7 +57,7 @@ resource "aws_instance" "docker1" {
   connection {
     type = "ssh"
     user = "ubuntu"
-    private_key = file ("/root/aws/aws5.pem")
+    private_key = file ("aws5")
     host = aws_instance.docker1.public_ip
     }
   }
@@ -88,7 +88,7 @@ resource "aws_instance" "docker1" {
     connection {
      type = "ssh"
      user = "ubuntu"
-     private_key = file ("/root/aws/aws5.pem")
+     private_key = file ("aws5")
      host = aws_instance.docker1.public_ip
     }
    }

e96e4f66659f92d4d47693fbcd639a6951307872
jonjesse <root@sys1.attlocal.net>
2020-10-20 16:11:23 -0500

    fixed

--- docker1.tf
+++ docker1.tf
@@ -45,7 +45,7 @@ resource "aws_instance" "docker1" {
   connection {
     type = "ssh"
     user = "ubuntu"
-    private_key = file ("aws5")
+    private_key = file ("~/.ssh/aws5.pem")
     host = aws_instance.docker1.public_ip
     }
   }
